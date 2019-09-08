101ccb1c518b1cec8f37ef16920140ea000a925e
St33v <github@f3rr3t.com>
2019-09-08 13:04:24 +1000

    Added ARGV encryption none

--- borgAuto.sh
+++ borgAuto.sh
@@ -2,14 +2,14 @@
 # 8 Sept 2019 SJ Pratt
 # Copied from https://blog.andrewkeech.com/posts/170718_borg.html
 # the envvar $REPONAME is something you should just hardcode
-export REPOSITORY="/mnt/bak/borg" 
+export REPOSITORY="/mnt/bak/st33vHome" 
 
 # Fill in your password here, borg picks it up automatically
-export BORG_PASSPHRASE="" 
+#export BORG_PASSPHRASE="" 
 
 # Backup all of /home except a few excluded directories and files
-borg create -v --stats --compression lz4                 \
-    $REPOSITORY::'{hostname}-{now:%Y-%m-%dT%H:%M}' /home \
+borg create -v --stats -e none --compression lz4                 \
+    $REPOSITORY::'{hostname}-{now:%Y-%m-%dT%H:%M}' /home/st33v \
 --exclude '/home/*/.cache'                               \
 --exclude '/home/$USER/cargo'                        \
 --exclude '/home/lost+found'                             \
